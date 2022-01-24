379b24ce15516195955154c7387b6b048f4b80c7
zainaltaf <zaltaf@uwaterloo.ca>
2022-01-24 18:32:35 -0500

    fix contact page hyperlink

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -16,14 +16,14 @@ function Contact() {
                     <br></br>
                     <ul>
                         <li>Email: y555zhu@uwaterloo.ca</li>
-                        <li>Github: <a style={{ color: "blue" }} href="github.com/theasdfone">github.com/theasdfone</a></li>
-                        <li>Linkedin: <a style={{ color: "blue" }} href="linkedin.com/in/theasdfone">linkedin.com/in/theasdfone</a></li>
+                        <li>Github: <a style={{ color: "blue", textDecoration: 'none'}} href="github.com/theasdfone">github.com/theasdfone</a></li>
+                        <li>Linkedin: <a style={{ color: "blue", textDecoration: 'none'}} href="linkedin.com/in/theasdfone">linkedin.com/in/theasdfone</a></li>
                     </ul>
                     <h2 style={{ marginTop: "50px" }}>Zain-Ul-Abideen Altaf</h2><br></br>
                     <ul style={{ marginBottom: "300px" }}>
                         <li>Email: zaltaf@uwaterloo.ca</li>
-                        <li>Github: <a style={{ color: "blue" }} href="https://github.com/zain-altaf">github.com/zain-altaf</a></li>
-                        <li>Linkedin: <a style={{ color: "blue" }} href="https://www.linkedin.com/in/zain-ul-abideen-altaf/">inkedin.com/in/zain-ul-abideen-altaf/</a></li>
+                        <li>Github: <a style={{ color: "blue", textDecoration: 'none'}} href="https://github.com/zain-altaf">github.com/zain-altaf</a></li>
+                        <li>Linkedin: <a style={{ color: "blue", textDecoration: 'none'}} href="https://www.linkedin.com/in/zain-ul-abideen-altaf/">inkedin.com/in/zain-ul-abideen-altaf/</a></li>
                     </ul>
                 </div>
             </div>
